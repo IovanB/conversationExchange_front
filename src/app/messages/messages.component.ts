5e17851f75a3e2d96cb2f2b5e6d0bb5f371d8965
Iovan Barros <iovanbarros@outlook.com>
2021-03-07 10:00:51 -0300

    update msg feature

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -37,4 +37,9 @@ export class MessagesComponent implements OnInit {
     this.loadMessages();
   }
 
+  deleteMessage(id:number){
+    this.messageService.deleteMessage(id).subscribe(() => {
+      this.messages.splice(this.messages.findIndex(m => m.id === id), 1);
+    })
+  }
 }
